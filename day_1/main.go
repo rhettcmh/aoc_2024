3098fd773f12d3453a9d6dc23b5b27821c1b8d70
Rhett <14178786@student.uts.edu.au>
2024-12-02 19:35:35 +1030

    Simplify day 1

--- day_1/main.go
+++ day_1/main.go
@@ -16,10 +16,11 @@ func main() {
 		panic(err)
 	}
 
-	// Read columns into slices
+	// Read columns into slices & counts
 	scanner := bufio.NewScanner(file)
 	var column_a []int
 	var column_b []int
+	counts := make(map[int]int)
 	for scanner.Scan() {
 		nums := strings.Fields(scanner.Text())
 		num_a, _ := strconv.Atoi(nums[0])
@@ -27,22 +28,20 @@ func main() {
 
 		num_b, _ := strconv.Atoi(nums[1])
 		column_b = append(column_b, num_b)
+		counts[num_b]++
 	}
 
-	// Sort slices into smallest -> largest
+	// Sorted slices
 	slices.Sort(column_a)
 	slices.Sort(column_b)
 
-	// Compute the differences
-	solution := 0.0
-	if len(column_a) == len(column_b) {
-		for i := range column_a {
-			solution += math.Abs(float64(column_a[i] - column_b[i]))
-		}
-	} else {
-		panic("The input data is malformed - column lengths are not equal.")
+	part_1 := 0.0
+	part_2 := 0.0
+	for i, number := range column_a {
+		part_1 += math.Abs(float64(column_a[i] - column_b[i]))
+		part_2 += float64(number * counts[number])
 	}
 
-	fmt.Println("The solution is:", int(solution))
-
+	fmt.Println("Part 1:", int(part_1))
+	fmt.Println("Part 2:", int(part_2))
 }
